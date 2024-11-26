de5746a82106cb40962223a0408eccf4b1b4524b
Bruno Bilheri Monteiro <brunobilheri@hotmail.com>
2024-11-26 15:01:08 -0300

    Create DB flask shell in terminal

--- app.py
+++ app.py
@@ -1,7 +1,20 @@
 # Importação
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///ecommerce.db'
+
+db = SQLAlchemy(app)
+
+# Modelagem
+# Produto (id, name, price, description)
+class Produto(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), nullable=False)
+    price = db.Column(db.Float, nullable=False)
+    description = db.Column(db.Text, nullable=True)    
+    
 
 # definição de rotas
 @app.route('/')
