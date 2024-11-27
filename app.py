7fd43571ec60f3dc2d4631add4664971924dbefc
Bruno Bilheri Monteiro <brunobilheri@hotmail.com>
2024-11-27 14:10:43 -0300

    route to the create product endpoint

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 # Importação
-from flask import Flask
+from flask import Flask, request
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
@@ -9,17 +9,26 @@ db = SQLAlchemy(app)
 
 # Modelagem
 # Produto (id, name, price, description)
-class Produto(db.Model):
+class Product(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(120), nullable=False)
     price = db.Column(db.Float, nullable=False)
-    description = db.Column(db.Text, nullable=True)    
-    
-
+    description = db.Column(db.Text, nullable=True) 
+      
 # definição de rotas
+@app.route('/api/products/add', methods=['POST'])
+def add_product():
+    data = request.json
+    product = Product(name=data["name"], price=data["price"], description=data.get("description", ""))
+    db.session.add(product)
+    db.session.commit()
+    return "Produto cadastrado com sucesso!"
+
 @app.route('/')
 def hello_world():
     return 'Hello World'
 
 if __name__ == "__main__":
     app.run(debug=True)
+    
+   
